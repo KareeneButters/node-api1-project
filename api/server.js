b9658c0ca7bb7befb8cd6f87f75fd610c2c352c9
Kareene Butler <Kareenebutler5@gmail.com>
2024-07-29 13:25:21 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
-// BUILD YOUR SERVER HERE
-
 const express = require('express')
 
 const server = express()
+
+server.get('/api/users', (req, res) => {
+res.json('users')
+})
 
 server.use('*', (req, res) => {
     res.status(404).json({
@@ -10,4 +12,4 @@ server.use('*', (req, res) => {
     })
 })
 
-module.exports = server; // EXPORT YOUR SERVER instead of {}
+module.exports = server; 
